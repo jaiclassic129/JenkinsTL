20f6e0851586234877e3df8352c2fb55ccee6a39
jaiclassic129 <56755047+jaiclassic129@users.noreply.github.com>
2021-09-25 15:14:38 +0530

    Update PrintConsole.java

--- ScheduleJob/src/job/PrintConsole.java
+++ ScheduleJob/src/job/PrintConsole.java
@@ -4,7 +4,7 @@ public class PrintConsole {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hurray, the second Jenkins Job - Updates1");
+		System.out.println("Webhooks Check");
 
 	}
 
